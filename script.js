b67d4ba16305b4619d3146c102e4aeab4614d6ce
Zinc <greenberg.zachary@utexas.edu>
2020-09-29 19:00:38 -0500

    Added Questions to js file

---
+++ script.js
@@ -0,0 +1,127 @@
+$(document).ready(function(){
+
+    var questions = [
+        {
+            questionNumber: 1,
+            questionText: "Commonly used data types DO NOT include:",
+            answers: [{
+              answerNumber: 1,
+              answerText: 'Strings',
+              answerCorrect: false
+            },
+            {
+              answerNumber: 2,
+              answerText: 'Booleans',
+              answerCorrect: false
+            },
+            {
+              answerNumber: 3,
+              answerText: "Alerts",
+              answerCorrect: true
+            },
+            {
+              answerNumber: 4,
+              answerText: 'Numbers',
+              answerCorrect: false
+            }],
+        },
+        {
+            questionNumber: 2,
+            questionText: "The condition in an if/else statement is enclosed within ____.",
+            answers: [{
+              answerNumber: 1,
+              answerText: 'Parentheses',
+              answerCorrect: true
+            },
+            {
+              answerNumber: 2,
+              answerText: 'Curly Brackets',
+              answerCorrect: false
+            },
+            {
+              answerNumber: 3,
+              answerText: "Quotes",
+              answerCorrect: false
+            },
+            {
+              answerNumber: 4,
+              answerText: 'Square Brackets',
+              answerCorrect: false
+            }],
+        },
+        {
+            questionNumber: 3,
+            questionText: "Arrays in JavaScript can be used to store ____.",
+            answers: [{
+              answerNumber: 1,
+              answerText: 'Numbers and Strings',
+              answerCorrect: false
+            },
+            {
+              answerNumber: 2,
+              answerText: 'Booleans',
+              answerCorrect: false
+            },
+            {
+              answerNumber: 3,
+              answerText: "Other Arrays",
+              answerCorrect: false
+            },
+            {
+              answerNumber: 4,
+              answerText: 'All of the above',
+              answerCorrect: true
+            }],
+        },
+        {
+            questionNumber: 4,
+            questionText: "String values must be enclosed within ____ when being assigned to variables.",
+            answers: [{
+              answerNumber: 1,
+              answerText: 'Commas',
+              answerCorrect: false
+            },
+            {
+              answerNumber: 2,
+              answerText: 'Curlry Brackets',
+              answerCorrect: false
+            },
+            {
+              answerNumber: 3,
+              answerText: "Quotes",
+              answerCorrect: true
+            },
+            {
+              answerNumber: 4,
+              answerText: 'Parenthesis',
+              answerCorrect: false
+            }],
+        },
+        {
+            questionNumber: 5,
+            questionText: "A very useful tool to use during development and debugging for printing content to the debugger is:",
+            answers: [{
+              answerNumber: 1,
+              answerText: 'JavaScript',
+              answerCorrect: false
+            },
+            {
+              answerNumber: 2,
+              answerText: 'Console Log',
+              answerCorrect: true
+            },
+            {
+              answerNumber: 3,
+              answerText: "Terminal/Bash",
+              answerCorrect: false
+            },
+            {
+              answerNumber: 4,
+              answerText: 'For Loops',
+              answerCorrect: false
+            }],
+        }
+    ];
+
+
+})
